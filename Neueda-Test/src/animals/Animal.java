56c65040ce426bd3f7ab776e198a39faeaa1d0e6
Will Parks <45174202+W-Parks@users.noreply.github.com>
2018-12-07 14:44:45 +0000

    removed wasted space

--- Neueda-Test/src/animals/Animal.java
+++ Neueda-Test/src/animals/Animal.java
@@ -2,4 +2,23 @@ package animals;
 
 public class Animal {
 
+	private String name;
+
+	public Animal() {
+		super();
+	}
+
+	public Animal(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
